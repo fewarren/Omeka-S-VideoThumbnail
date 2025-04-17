9d872b9827e633a3b21ac9a51fa2700c9a21bdf3
fewarren <FWarren574@aol.com>
2025-04-17 08:16:53 -0700

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,3 @@
-<?php
 namespace VideoThumbnail;
 
 return [
@@ -15,17 +14,61 @@ return [
     'form_elements' => [
         'factories' => [
             Form\ConfigForm::class => Service\Form\ConfigFormFactory::class,
-            Form\ConfigBatchForm::class => Service\Form\ConfigFormFactory::class,
         ],
     ],
     'controllers' => [
         'factories' => [
-            'VideoThumbnail\Controller\Admin\VideoThumbnailController' => Service\Controller\VideoThumbnailControllerFactory::class,
-            // Add alias for short name
-            'VideoThumbnailController' => Service\Controller\VideoThumbnailControllerFactory::class,
+            'VideoThumbnail\Controller\Admin\VideoThumbnail' => Service\Controller\VideoThumbnailControllerFactory::class,
         ],
     ],
     'controller_plugins' => [
         'factories' => [
-            'extractVideoFrames' => Service\ControllerPlugin\ExtractVideoFramesFactory::class](#)
+            'extractVideoFrames' => Service\ControllerPlugin\ExtractVideoFramesFactory::class,
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'admin' => [
+                'child_routes' => [
+                    'video-thumbnail' => [
+                        'type' => 'Segment',
+                        'options' => [
+                            'route' => '/video-thumbnail[/:action]',
+                            'defaults' => [
+                                '__NAMESPACE__' => 'VideoThumbnail\Controller\Admin',
+                                'controller' => 'VideoThumbnail',
+                                'action' => 'index',
+                            ],
+                        ],
+                    ],
+                    'video-thumbnail-select-frame' => [
+                        'type' => 'Segment',
+                        'options' => [
+                            'route' => '/video-thumbnail/select-frame/:id',
+                            'defaults' => [
+                                '__NAMESPACE__' => 'VideoThumbnail\Controller\Admin',
+                                'controller' => 'VideoThumbnail',
+                                'action' => 'select-frame',
+                            ],
+                            'constraints' => [
+                                'id' => '\d+',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'navigation' => [
+        'AdminModule' => [
+            [
+                'label' => 'Video Thumbnail', 
+                'route' => 'admin/video-thumbnail',
+                'resource' => 'VideoThumbnail\Controller\Admin\VideoThumbnail',
+            ],
+        ],
+    ],
+    'media_ingesters' => [
+        'factories' => [
+            '](#)
 
